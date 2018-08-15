11d42dbfe514e27d985b7a20a375e54b8dd274a7
Maverick Tesini <mavericktesini@gmail.com>
2018-08-15 01:14:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,13 +4,13 @@ function init() {
   let index = 0;
   document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.key || e.detail); //key superceds which in current documentation
-
+    //does keydown press equal current indexed value of code?
     if (key === code[index]) {
       index++;
-
+      //after progressing the current index value (due to match), check if all code values have been used
       if (index === code.length) {
         alert('Hurray!');
-
+        //then reset the code's index
         index = 0;
       }
     } else {
